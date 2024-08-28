207afd85b6ea85db75b9e4eba0ab2e44fe6a4b15
giho <ff6022@naver.com>
2024-08-28 15:14:45 +0900

    header search_box design complete

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import style from "../styles/header.module.css"
+import { FaSearch } from "react-icons/fa";
+
 
 const Header:React.FC = () => {
   return (
@@ -9,13 +11,19 @@ const Header:React.FC = () => {
           <h1>대여돼요</h1>
         </div>
         <div className={style.header_search}>
-          <label className={style.search_box}>
+          <div className={style.search_box}>
             {/* <div className='search-input-box'>
               <input type="text" />
             </div> */}
-            <input type="text" />
-            <div className='search-button'>검색</div>
-          </label>
+            <label className={style.search_input_label}>
+              <input type="text" className={style.search_input} placeholder='검색어를 입력하세요.' />
+            </label>
+            <label className={style.search_button_label}>
+              <button className={style.search_button}>
+                <FaSearch />
+              </button>
+            </label>
+          </div>
         </div>
         <div className={style.header_user}>
           <div className={style.user_image}>이미지</div>
